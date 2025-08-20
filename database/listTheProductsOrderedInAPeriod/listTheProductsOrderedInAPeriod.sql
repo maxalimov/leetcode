7a296d70dee19ebf39847c77170beccc4ed78b4a
Maksat Alimov <maxalimovkk51163@gmail.com>
2025-08-20 12:39:14 +0600

    Added solution for LeetCode #1327

---
+++ database/listTheProductsOrderedInAPeriod/listTheProductsOrderedInAPeriod.sql
@@ -0,0 +1,8 @@
+SELECT
+    product_name,
+    SUM(unit) AS unit
+FROM Products
+JOIN Orders ON Products.product_id = Orders.product_id
+WHERE Orders.order_date >= '2020-02-01' AND Orders.order_date < '2020-03-01'
+GROUP BY Products.product_id
+HAVING SUM(Orders.unit) >= 100;
