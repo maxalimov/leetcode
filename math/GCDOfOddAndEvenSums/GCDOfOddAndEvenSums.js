17661e0a440e6685d8fa19dd3b916a3fd707798a
Maksat Alimov <maxalimovkk51163@gmail.com>
2025-08-29 17:50:20 +0600

    Added solution for LeetCode #3658

---
+++ math/GCDOfOddAndEvenSums/GCDOfOddAndEvenSums.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// const GCD = (a, b) => {
+//     while (b !== 0) {
+//         const temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+
+//     return a;
+// };
+
+// const gcdOfOddEvenSums = function(n) {
+//     const oddSum = n * n;
+//     const evenSum = n * (n + 1);
+
+//     return GCD(evenSum, oddSum);
+// };
+
+const gcdOfOddEvenSums = (n) => {
+    return n
+};
+
+//Constraints:
+// 1 <= n <= 1000
