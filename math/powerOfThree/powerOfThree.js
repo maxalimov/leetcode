90937deaa68f440de41e2afea7b50717c76eaf6e
Maksat Alimov <maxalimovkk51163@gmail.com>
2025-11-29 16:56:42 +0600

    Added solution for LeetCode #326

---
+++ math/powerOfThree/powerOfThree.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThree = function(n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    while (n % 3 === 0) {
+        n /= 3;
+    }
+
+    return n === 1;
+};
+
+//Constraints:
+// -231 <= n <= 231 - 1
